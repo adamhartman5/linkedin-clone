62ff22213735875ceab13235e92b7b72912419a9
Adam Hartman <adamhartman5@gmail.com>
2023-03-09 01:30:33 -0500

    Finish Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,13 @@ import './Sidebar.css';
 import { Avatar } from '@mui/material';
 
 function Sidebar() {
+
+	const recentItem = (topic) => (
+		<div className="sidebar__recentItem">
+			<span className="sidebar__hash">#</span>
+			<p>{topic}</p>
+		</div>
+	)
 	return (
 		<div className='sidebar'>
 			<div className='sidebar__top'>
@@ -17,16 +24,21 @@ function Sidebar() {
 			<div className='sidebar__stats'>
 				<div className='sidebar__stat'>
 					<p>Who viewed you</p>
-					<p className='sidebar__statnumber'>2,543</p>
+					<p className='sidebar__statNumber'>2,543</p>
 				</div>
 				<div className='sidebar__stat'>
 					<p>Views on post</p>
-					<p className='sidebar__statnumber'>2,448</p>
+					<p className='sidebar__statNumber'>2,448</p>
 				</div>
 			</div>
 
 			<div className='sidebar__bottom'>
 				<p>Recent</p>
+				{recentItem('reactjs')}
+				{recentItem('programming')}
+				{recentItem('softwareengineering')}
+				{recentItem('design')}
+				{recentItem('developer')}
 			</div>
 	</div>
 	);
