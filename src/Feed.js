a4b7cf106383984a6554c13c5e328f29309bade8
Adam Hartman <adamhartman5@gmail.com>
2023-03-09 23:08:59 -0500

    Start creating feed

--- src/Feed.js
+++ src/Feed.js
@@ -8,6 +8,10 @@ function Feed() {
 			<div className='feed__inputContainer'>
 				<div className='feed__input'>
 					<CreateIcon />
+					<form>
+						<input type="text" />
+						<button type='submit'>Send</button>
+					</form>
 				</div>
 			</div>
 		</div>
